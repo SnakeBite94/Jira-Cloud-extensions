fe8f3a7c07b5a900ed839332cb14857d4ebc8eb7
SnakeBite94 <64260006+SnakeBite94@users.noreply.github.com>
2022-05-23 11:16:44 +0200

    Use "Standard" waitForKeyElements, new extensions for checklists

--- JiraCloudExtensions.js
+++ JiraCloudExtensions.js
@@ -1,25 +1,39 @@
 // ==UserScript==
 // @name         Jira Cloud extensions
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Copy Jira Cloud issue key and description to clipboard; Set description editor max height, so the toolbar stays visible;
 // @author       Dejf
 // @match        https://*/browse/*
+// @match        https://issue-checklist-free-1.herocoders.com/*
 // @icon         https://www.google.com/s2/favicons?domain=atlassian.net
-// @grant        none
+// @grant        GM_addStyle
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @downloadURL  https://raw.githubusercontent.com/SnakeBite94/Jira-Cloud-extensions/main/JiraCloudExtensions.js
 // ==/UserScript==
 
 (function () {
     'use strict';
-    //GM_addStyle('.ak-editor-content-area { max-height: 480px; }');
-    addToolbarButton('📋 To clipboard', copyJiraKeyAndDescriptionToClip);
-    addCollapseRightPanelButton();
+    if (window.top != window.self) // iframe
+    {
+        if (window.name.includes("editor-dialog"))
+        {
+            waitForKeyElements('.css-h2743', e=> {
+                e.css('margin-left', '50px');
+            }, true); // true=onlyonce
+        }
+    }
+    else // main page
+    {
+        addToolbarButton('📋 To clipboard', copyJiraKeyAndDescriptionToClip);
+        addCollapseRightPanelButton();
+    }
 })();
 
 function addCollapseRightPanelButton() {
-    whenAvailable("jira-issue-header-actions", function () {
-        const rightPanel = document.getElementById("jira-issue-header-actions").parentElement.parentElement;
+    waitForKeyElements(".mffpf0-2.GTSJf", p => {
+        const rightPanel = p[0];
         let btn = document.createElement('button')
         btn.innerHTML = "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" role=\"presentation\"><path d=\"M10.294 9.698a.988.988 0 010-1.407 1.01 1.01 0 011.419 0l2.965 2.94a1.09 1.09 0 010 1.548l-2.955 2.93a1.01 1.01 0 01-1.42 0 .988.988 0 010-1.407l2.318-2.297-2.327-2.307z\" fill=\"currentColor\" fill-rule=\"evenodd\"></path></svg>";
         btn.id = "collapse-right-panel";
@@ -49,22 +63,19 @@ function addCollapseRightPanelButton() {
             rightPanel.style.display = "none";
         }
         rightPanel.insertBefore(btn, rightPanel.firstChild)
-    });
+    }, true);
 }
 
 function addToolbarButton(text, onclick, cssObj) {
-    whenAvailable('jira-issue-header', function () {
+    waitForKeyElements('.um2giz-0.guBgLL', target=> {
         let button = document.createElement('button');
-        let target = document.getElementById('jira-issue-header')
-            .nextElementSibling.nextElementSibling.nextElementSibling;
-        let cls = target.getElementsByTagName('button')[0].className;
-
         button.innerHTML = text;
         button.onclick = onclick;
         button.onsubmit = () => { };
-        button.className = cls;
-        target.appendChild(button);
-    });
+        button.className = "css-vhhexr";
+        button.style = "margin-left: 10px";
+        target.append(button);
+    }, true);
 }
 
 function copyJiraKeyAndDescriptionToClip() {
@@ -83,31 +94,4 @@ function toClipboard(text) {
     textArea.select();
     document.execCommand("Copy");
     textArea.remove();
-}
-
-function whenAvailable(name, callback) {
-    let interval = 10; // ms
-    window.setTimeout(function () {
-        if (window[name]) {
-            callback(window[name]);
-        } else {
-            whenAvailable(name, callback);
-        }
-    }, interval);
-}
-
-function getElementsByText(str, tag = 'a') {
-    return Array.prototype.slice.call(document.getElementsByTagName(tag)).filter(el => el.textContent.trim() === str.trim());
-}
-
-function GM_addStyle(css) {
-    const style = document.getElementById("GM_addStyleBy8626") || (function () {
-        const style = document.createElement('style');
-        style.type = 'text/css';
-        style.id = "GM_addStyleBy8626";
-        document.head.appendChild(style);
-        return style;
-    })();
-    const sheet = style.sheet;
-    sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
 }
